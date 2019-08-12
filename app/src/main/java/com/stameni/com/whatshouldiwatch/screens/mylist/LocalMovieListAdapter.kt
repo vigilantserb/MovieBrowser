e37342196e8b6ac8607648218439c45ed22b76f1
Mihajlo S <mihajlo.stamenkovic95@gmail.com>
2019-08-12 16:40:16 +0200

    Swipe right to reveal functions on my list fragment elements

--- app/src/main/java/com/stameni/com/whatshouldiwatch/screens/mylist/LocalMovieListAdapter.kt
+++ app/src/main/java/com/stameni/com/whatshouldiwatch/screens/mylist/LocalMovieListAdapter.kt
@@ -8,8 +8,8 @@ import android.view.animation.AlphaAnimation
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.stameni.com.whatshouldiwatch.R
+import com.stameni.com.whatshouldiwatch.common.Constants
 import com.stameni.com.whatshouldiwatch.common.ImageLoader
-import com.stameni.com.whatshouldiwatch.common.listen
 import com.stameni.com.whatshouldiwatch.data.room.models.Movie
 import com.stameni.com.whatshouldiwatch.screens.singleMovie.SingleMovieActivity
 import kotlinx.android.synthetic.main.my_list_movie_item.view.*
@@ -21,14 +21,7 @@ class LocalMovieListAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.my_list_movie_item, parent, false)
-        return ViewHolder(v, parent).listen { position, type ->
-            val item = items[position]
-            val intent = Intent(parent.context, SingleMovieActivity::class.java)
-            intent.putExtra("posterUrl", item.movieImageUrl)
-            intent.putExtra("movieId", item.movieId)
-            intent.putExtra("movieName", item.movieName)
-            parent.context.startActivity(intent)
-        }
+        return ViewHolder(v, parent)
     }
 
     override fun getItemCount(): Int {
@@ -47,27 +40,30 @@ class LocalMovieListAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var listItem = items[position]
+        val listItem = items[position]
         holder.movieTitle.text = listItem.movieName
         holder.movieGenres.text = listItem.movieGenres
         holder.movieYear.text = "1989"
         holder.movieTitle.text = listItem.movieName
         val url = listItem.movieImageUrl
+
         holder.infoButton.setOnClickListener { v ->
-            Toast.makeText(
-                v.context,
-                "INFO CLICKED",
-                Toast.LENGTH_SHORT
-            ).show()
+            Toast.makeText(v.context, "INFO CLICKED", Toast.LENGTH_SHORT).show()
         }
         holder.editButton.setOnClickListener { v ->
-            Toast.makeText(
-                v.context,
-                "EDIT CLICKED",
-                Toast.LENGTH_SHORT
-            ).show()
+            Toast.makeText(v.context, "EDIT CLICKED", Toast.LENGTH_SHORT).show()
         }
-        if(url != null) imageLoader.loadPosterImageCenterCrop(url, holder.moviePoster, "w92")
+
+        holder.movieView.setOnClickListener {
+            val item = items[position]
+            val intent = Intent(it.context, SingleMovieActivity::class.java)
+            intent.putExtra(Constants.POSTER_URL, item.movieImageUrl)
+            intent.putExtra(Constants.MOVIE_ID, item.movieId)
+            intent.putExtra(Constants.MOVIE_NAME, item.movieName)
+            it.context.startActivity(intent)
+        }
+
+        if (url != null) imageLoader.loadPosterImageCenterCrop(url, holder.moviePoster, "w92")
         holder.setFadeAnimation(holder.itemView)
     }
 
@@ -78,6 +74,7 @@ class LocalMovieListAdapter(
         var movieGenres = itemView.genres_text_view
         var infoButton = itemView.info_button
         var editButton = itemView.edit_button
+        var movieView = itemView.movie_root
 
         fun setFadeAnimation(view: View) {
             val anim = AlphaAnimation(0.0f, 1.0f)
