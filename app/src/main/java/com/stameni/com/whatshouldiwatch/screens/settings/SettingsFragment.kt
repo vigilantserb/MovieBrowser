0242346272038411885d3d991760d31cb81c8dc9
Mihajlo S <mihajlo.stamenkovic95@gmail.com>
2019-08-19 16:36:16 +0200

    Export movie lists to CSV format

--- app/src/main/java/com/stameni/com/whatshouldiwatch/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/stameni/com/whatshouldiwatch/screens/settings/SettingsFragment.kt
@@ -1,26 +1,46 @@
 package com.stameni.com.whatshouldiwatch.screens.settings
 
+import android.Manifest
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
+import com.karumi.dexter.listener.single.PermissionListener
 import com.stameni.com.whatshouldiwatch.R
 import com.stameni.com.whatshouldiwatch.common.baseClasses.BaseFragment
+import com.stameni.com.whatshouldiwatch.common.libraries.CSVWriter
+import com.stameni.com.whatshouldiwatch.data.room.MovieDatabase
 import com.stameni.com.whatshouldiwatch.screens.settings.about.AboutUsActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_settings_new.*
+import timber.log.Timber
 import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 import javax.inject.Inject
 
 class SettingsFragment : BaseFragment() {
 
     @Inject
     lateinit var prefs: SharedPreferences
+
+    @Inject
+    lateinit var movieDatabase: MovieDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -60,6 +80,76 @@ class SettingsFragment : BaseFragment() {
         to_watch_placeholder.setOnClickListener {
             startActivity(Intent(context, AboutUsActivity::class.java))
         }
+
+        save_your_lists_placeholder.setOnClickListener {
+
+            Dexter.withActivity(activity)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(object : PermissionListener{
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        createCsvFile()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permission: PermissionRequest?,
+                        token: PermissionToken?
+                    ) {
+                        token!!.continuePermissionRequest()
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        DialogOnDeniedPermissionListener.Builder
+                            .withContext(context)
+                            .withTitle("Camera permission")
+                            .withMessage("Camera permission is needed to take pictures of your cat")
+                            .withButtonText(android.R.string.ok)
+                            .withIcon(R.mipmap.ic_new_launcer)
+                            .build()
+                    }
+
+                }).check()
+        }
+    }
+
+    private fun createCsvFile(): Disposable {
+        val exportDir = File(Environment.getExternalStorageDirectory().path)
+        if (!exportDir.exists()) {
+            exportDir.mkdirs()
+        }
+
+        val file = File(exportDir, "Movies.csv")
+        if(!file.exists()){
+            try {
+                file.createNewFile()
+            }catch (e: IOException){
+                Timber.e(e)
+            }
+        }
+        val csvWrite = CSVWriter(FileWriter(file))
+        val column = arrayOf("movieTitle", "releaseDate", "movieGenres", "movieImageUrl", "movieId", "listType")
+        csvWrite.writeNext(column)
+
+        return movieDatabase.movieDao()
+            .getMovies()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ movies ->
+                for (i in movies.indices) {
+                    val mySecondStringArray =
+                        arrayOf(
+                            movies[i].movieTitle!!,
+                            movies[i].releaseDate!!,
+                            movies[i].movieGenres!!,
+                            movies[i].movieImageUrl!!,
+                            movies[i].movieId.toString(),
+                            movies[i].listType!!
+                        )
+                    csvWrite.writeNext(mySecondStringArray)
+                }
+                csvWrite.close()
+            }, {
+                Timber.e(it)
+            })
     }
 
     private fun handleImageCache() {
