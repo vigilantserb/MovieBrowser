3483dcaf45ff25a50160f75f8c45a893b11e32f5
Mihajlo Stamenkovic <mihajlo.stamenkovic95@gmail.com>
2024-05-25 16:19:06 +0200

    refactor fetching data for lists of top movies

--- app/src/main/java/com/stameni/com/moviebrowser/screens/discover/topLists/TopListMoviesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.stameni.com.moviebrowser.screens.discover.topLists
-
-import androidx.lifecycle.ViewModel
-
-class TopListMoviesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
