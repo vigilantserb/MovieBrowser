d0ce267cab3306db9ce4ad24e8aa035905e8f414
Михајло Стаменковић <37147378+vigilantserb@users.noreply.github.com>
2024-06-13 22:17:20 +0200

    Refactor intent sending to be composable (#7)
    Co-authored-by: Mihajlo Stamenkovic <mihajlo.stamenkovic95@gmail.com>

--- app/src/main/java/com/stameni/com/moviebrowser/common/MessageGenerator.kt
+++ app/src/main/java/com/stameni/com/moviebrowser/common/MessageGenerator.kt
@@ -3,20 +3,18 @@ package com.stameni.com.moviebrowser.common
 import android.content.Intent
 import com.stameni.com.moviebrowser.data.models.movie.MovieDetails
 
-class MessageGenerator {
-    companion object{
-        fun shareMovieMessageIntent(movieDetails: MovieDetails): Intent {
-            var movieDate = ""
-            if (movieDetails.releaseDate != null) {
-                movieDate = movieDetails.releaseDate.removeRange(4, movieDetails.releaseDate.length)
+class MessageGenerator(private val intentGenerator: IntentGenerator) {
+
+    fun shareMovieMessageIntent(movieDetails: MovieDetails): Intent {
+        movieDetails.apply {
+            val movieDate = releaseDate?.let {
+                releaseDate.removeRange(4, releaseDate.length)
             }
-            val string =
-                "Hey! Check out ${movieDetails.movieTitle} | $movieDate directed by ${movieDetails.directorName}. Rated ${movieDetails.tmdbRating} on TMDB!"
-            return Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, string)
-                type = "text/plain"
-            }
+
+            val txtMsg =
+                "Hey! Check out $movieTitle | $movieDate " +
+                        "directed by $directorName. Rated $tmdbRating on TMDB!"
+            return intentGenerator.sendTextMessageIntent(txtMsg)
         }
     }
 }
